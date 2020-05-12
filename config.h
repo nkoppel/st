6464dd3a7e6673547ee53261cb9814d59a40f029
Nathan Koppel <nathankoppel0@gmail.com>
2020-05-12 13:26:03 -0500

    fix merge conflicts

--- config.h
+++ config.h
@@ -122,24 +122,11 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 unsigned int defaultfg = 12;
 unsigned int defaultbg = 8;
 static unsigned int defaultcs = 14;
 static unsigned int defaultrcs = 15;
-=======
-=======
->>>>>>> ligature-boxdraw
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
-<<<<<<< HEAD
->>>>>>> scrollback
-=======
->>>>>>> ligature-boxdraw
 
 /*
  * Default shape of cursor
@@ -184,11 +171,6 @@ static uint forcemousemod = ShiftMask;
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
-<<<<<<< HEAD
-<<<<<<< HEAD
-	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
-	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
-=======
 	{ XK_NO_MOD,           Button4, ttysend,        {.s = "\031"} },
 	{ XK_NO_MOD,           Button5, ttysend,        {.s = "\005"} },
 };
@@ -197,11 +179,6 @@ MouseKey mkeys[] = {
 	/* button               mask            function        argument */
 	{ Button4,              ShiftMask,      kscrollup,      {.i =  1} },
 	{ Button5,              ShiftMask,      kscrolldown,    {.i =  1} },
->>>>>>> scrollback
-=======
-	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
-	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
->>>>>>> ligature-boxdraw
 };
 
 /* Internal keyboard shortcuts. */
@@ -220,14 +197,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
->>>>>>> scrollback
-=======
->>>>>>> ligature-boxdraw
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 };
